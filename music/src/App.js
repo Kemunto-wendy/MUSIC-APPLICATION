b1b39f5dc7a250f7d611e17e48d493b89430b30a
JOY <joy.kemunto@student.moringaschool.com>
2022-07-27 23:02:27 +0300

    changes

--- music/src/App.js
+++ music/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import SearchInput from './components/Searchinput/search';
 
 function App() {
   return (
     <div className="App m-20"> 
-    <form id="form flex justify-sb">
+    <form>
     <input type="text" id="search" class="search" placeholder="Type song Name"></input>
-   </form> 
+    </form>
       <div className="title">
          <h2>SEARCH FOR<span> THE BEST</span> MUSIC</h2>
             <h5><span>Music doesn't lie. </span> If there is something to be changed in this world, then it can only happen through music.</h5>
-      </div>
+      <SearchInput />
+            </div>
     </div>
 
   );
---
+++ music/src/components/Searchinput/search.js
@@ -0,0 +1,10 @@
+import './search.css'
+const SearchInput = props => {
+    return(
+        <div>
+            <input />
+        </div>
+    )
+}
+
+export default SearchInput;
